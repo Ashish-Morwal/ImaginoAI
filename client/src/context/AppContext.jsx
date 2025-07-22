ba45213d814ac090c53c81e61abddb66dc943534
Ashish Morwal <126047374+Ashish-Morwal@users.noreply.github.com>
2025-07-22 20:50:09 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -11,7 +11,8 @@ const AppContextProvider = (props) => {
   const [token, setToken] = useState(localStorage.getItem("token"));
 
   const [credit, setCredit] = useState(false);
-  const backendUrl = 'https://imaginoai-server.onrender.com';
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
 
   const navigate = useNavigate();
 
